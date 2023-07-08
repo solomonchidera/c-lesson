2ef7a7384b8d8a0f1c915e3fcd5b40e3237e77d7
SolomonChidera <solochidera10@gmail.com>
2023-07-08 03:35:22 +0100

    basic file that was used for explaination

---
+++ cprogram/basic_structure/basic.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+/**
+ * THIS FILE WAS USED TO EXPLAIN THE BASIC STRUCTURE OF A C FILE
+ * COMPILING THE CODE WASN'T THE GOAL IN THIS FILE
+ *
+ * syntax is a particular way of arranging words in a laguage
+ * syntax of a function in c
+
+ * return_type function_name (void,empty)
+ */
+void main(void)
+{
+
+	printf("Testing headerfile");
+}
+
+/* syntax in python*/
+
+print("Hello world");// YOU CAN TERMINATE A LINE IN PYTHON OR NOT (;)
