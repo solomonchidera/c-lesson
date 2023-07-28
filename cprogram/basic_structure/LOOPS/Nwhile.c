b1beb357d3bdb95e21885fd92fe60271f1a9c11f
SolomonChidera <solochidera10@gmail.com>
2023-07-28 22:44:42 +0100

    Another file for nested-while loops

---
+++ cprogram/basic_structure/LOOPS/Nwhile.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+int main(void)
+{
+	int a = 0;
+	char b;
+
+
+	while(a <= 10)
+	{
+		b = 'a';
+		while(b < 'z')
+		{
+			putchar(b);
+			b++;
+		}
+		putchar(10);
+		a++;
+	}
+	return(0);
+}
